fc78a6c79e2b3be3f65b7d7b68fd670940aebab4
Jon Lucas <jon.s.lucas@gmail.com>
2016-02-09 16:28:17 -0500

    Can't forget the unix timestamp stuffs.

--- app/controllers/timeStampHandler.server.js
+++ app/controllers/timeStampHandler.server.js
@@ -5,20 +5,34 @@ let moment = require('moment');
 function tsHandler() {
     // convert natural language date/time into Date obj
     this.handleTime = (req, res)=>{
-        let date = Date.parse(req.params.timestamp);
+        let timestamp = req.params.timestamp;
+        let date = Date.parse(timestamp);
         if (isNaN(date)){
-            res.json({
-                'unix': null,
-                'natural': null
-            });
-        } else {
-            date = new Date(date);
-            res.json({
-                'unix': date.getTime(),
-                'natural': moment(date).format('MMMM Do YYYY')
-            });
-        }
+            // in unable to parse, i.e. either a unix time or invalid
+            if (timestamp.match(/[^\d]+/)){
+                // param was invalid
 
+                res.json({
+                    'unix': null,
+                    'natural': null
+                });
+            } else {
+                // param was a unix time string;
+                // don't forget to convert sec to ms
+                date = new Date(parseInt(timestamp)*1000);
+
+            }
+
+        } else {
+            // param was natural language date
+            date = new Date(date);
+
+        }
+        // don't for get to convert ms to sec
+        res.json({
+            'unix': date.getTime()/1000,
+            'natural': moment.utc(date).format('MMMM Do YYYY')
+        });
     };
 
 };
